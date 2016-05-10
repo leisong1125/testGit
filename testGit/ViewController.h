57898912cef84d09140178abf428345368bfa11b
FanLang <fanlang@fanlangdemacbook-air.local>
2016-05-10 11:52:44 +0800

    Initial Commit

---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by FanLang on 16/5/10.
+//  Copyright © 2016年 FanLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testGit
+//
+//  Created by FanLang on 16/5/10.
+//  Copyright © 2016年 FanLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
